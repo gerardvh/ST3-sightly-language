e88736cc24f5c1216abca83468370aac11e778b0
Gerard Van Halsema <gerardvh@me.com>
2017-02-10 10:29:48 -0600

    Add uri manipulation completions.

--- sightly_completions.py
+++ sightly_completions.py
@@ -1,10 +1,6 @@
 import sublime
 import sublime_plugin
 
-def make_context_completion(context):
-    contents = 'context=\'{0}\''.format(context)
-    return make_completion(context, 'Display Context', contents)
-
 def make_completion(trigger, label, contents):
     full_trigger = '{0}\t{1}'.format(trigger, label)
     return (full_trigger, contents)
@@ -19,10 +15,30 @@ class SightlyCompletions(sublime_plugin.EventListener):
         if not view.match_selector(locations[0], "source.sightly.embedded.html"):
             return []
 
-        in_options = view.match_selector(locations[0], self.option_list_scope)
+        in_options = view.match_selector(locations[0] - 1, self.option_list_scope)
         in_expression = view.match_selector(locations[0], self.expression_scope)
+        return self.get_completions(view, prefix, locations, in_options)
 
-        return (self.display_context_completions(), sublime.INHIBIT_WORD_COMPLETIONS)
+    def get_completions(self, view, prefix, locations, in_options):
+        pt = locations[0] - len(prefix) - 1
+        ch = view.substr(sublime.Region(pt, pt + 1))
+
+        # print('prefix:', prefix)
+        # print('location0:', locations[0])
+        # print('substr:', view.substr(sublime.Region(locations[0], locations[0] + 3)), '!end!')
+        # print('ch:', ch)
+
+        if in_options:
+            return (self.default_options(), sublime.INHIBIT_WORD_COMPLETIONS)
+        else:
+            return []
+
+    def default_options(self):
+        # basic completions for built-in options ie. `context='$1'` or ``prependPath='$1'`
+        default_completions = []
+        default_completions.extend(self.display_context_completions())
+        default_completions.extend(self.uri_manipulation_completions())
+        return default_completions
 
     def display_context_completions(self):
         contexts = [
@@ -32,8 +48,13 @@ class SightlyCompletions(sublime_plugin.EventListener):
             'unsafe'
         ]
 
-        return [make_context_completion(c) for c in contexts]
+        return [make_completion(c, 'Display Context', "context='{0}'".format(c)) for c in contexts]
 
-    def default_completions(self):
-        # basic completions for built-in options ie. `context='$1'` or ``prependPath='$1'`
-        pass
+    def uri_manipulation_completions(self):
+        options = [
+            'scheme', 'domain', 'path', 'prependPath', 'appendPath', 'selectors',
+            'addSelectors', 'removeSelectors', 'extension', 'suffix', 'prependSuffix',
+            'appendSuffix', 'query', 'addQuery', 'removeQuery', 'fragment'
+        ]
+
+        return [make_completion(o, 'URI Options', "{0}='$1'".format(o)) for o in options]
